e53c6070a3c0626276cdc166d1c71cc2eaa807cb
ThomasA64 <thomas.arthur64@gmail.com>
2020-11-13 20:57:10 +0000

    added delete button

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -59,6 +59,13 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = 
+  console.log('url',urlDatabase);
+  const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req,res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+})
+
