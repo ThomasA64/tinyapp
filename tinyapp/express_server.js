017fbeb2f72559dac3142fc8af04da6f80292654
ThomasA64 <thomas.arthur64@gmail.com>
2020-11-16 01:31:12 +0000

    added login/logout

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
 
 const generateRandomString = function () {
   const randString = Math.random().toString(36).substring(2,8);
@@ -15,6 +20,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -33,8 +51,9 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
+  // console.log(req.cookies("User_id"));
 });
 
 app.get("/urls/new", (req, res) => {
@@ -69,3 +88,30 @@ app.post("/urls/:shortURL/delete", (req,res) => {
   res.redirect("/urls")
 })
 
+app.get("/register", (req, res) => {
+  res.render("register")
+})
+
+app.post("/register", (req,res) => {
+  console.log(req.body, "req");
+  const newuserId = generateRandomString()
+  users[newuserId] = {id: newuserId, email: req.body.email, password: req.body.password};
+  console.log(users);
+  res.cookie("User_id", newuserId);
+  res.redirect("/urls")
+})
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username)
+  res.redirect("/urls")
+  // res.cookie("username", users["email", "password"])
+  // Get the form data from the request, email and password
+  // Authenticate the user
+  // If the user authenicates successfully, then we should send them a cookie, if not send them back an error.
+  // res.redirect("/urls")
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls")
+})
