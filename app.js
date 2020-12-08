ca3e5e7a08c64162733a87d9c72e017c6fa28427
ThomasA64 <thomas.arthur64@gmail.com>
2020-12-08 22:58:05 +0000

    Added the cookies feature to TinyApp

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const http = require("http");
 const PORT = 8080;
 
-// a function which handles requests and sends response
+// a function that handles requests and sends response
+
 const requestHandler = function(request, response) {
-  if (request.url === "/") {
-    response.end("Welcome!");
-  } else if (request.url === "/urls") {
-    response.end("www.lighthouselabs.ca\nwww.google.com");
-  } else {
-    response.statusCode = 404;
-    response.end("404 Page Not Found");
-  }
+  console.log('In requestHandler'); // NEW LINE
+  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
 };
 
 const server = http.createServer(requestHandler);
-console.log('Server created');
+console.log('Server created'); // NEW LINE
+
 
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
 
-console.log('Last line (after .listen call)');
+console.log('Last line (after .listen call)'); // NEW LINE
