8670a699e43dc34a13057830a880a13277e6df1a
ThomasA64 <thomas.arthur64@gmail.com>
2020-12-09 23:26:14 +0000

    added Basic Permission Features so the user logged in is the only one to see the ShortURLS they created.

--- express_server.js
+++ express_server.js
@@ -16,20 +16,20 @@ const generateRandomString = function () {
   return randString;
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-  //* STEP 2 for Basic Permission Features: 
-//* Change urlDatabase object structure to: 
-//* "b2xVn2": {"longUrl: http://www.lighthouselabs.ca", userID: users[req.cookies["user_id"]]}
-//? Maybe it would be users[req.cookies["user_id"]]
-//* or users.id
-};
+// const OLDurlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+//   //* STEP 2 for Basic Permission Features: 
+// //* Change urlDatabase object structure to: 
+// //* "b2xVn2": {"longUrl: http://www.lighthouselabs.ca", userID: users[req.cookies["user_id"]]}
+// //? Maybe it would be users[req.cookies["user_id"]]
+// //* or users.id
+// };
 
-// const urlDatabase = {
-//   "b2xVn2": {longUrl: "http://www.lighthouselabs.ca", userID: [req.cookies["user_id"]]},
-//   "9sm5xK": {longUrl: "http://www.google.com", userID: [req.cookies["user_id"]]}
-//   };
+const urlDatabase = {
+  "b2xVn2": {longUrl: "http://www.lighthouselabs.ca", userID: "exampleUser1"},
+  "9sm5xK": {longUrl: "http://www.google.com", userID: "exampleUser2"}
+  };
 
 const users = { 
   "userRandomID": {
@@ -43,8 +43,6 @@ const users = {
     password: "dishwasher-funk"
   }
 }
-
-
 
 const findUserByEmail = function(email) {
   // 1st step: 
@@ -73,15 +71,27 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+const UrlofUser = function(ID) {
+  const Obj = {}
+  for (const shortURL in urlDatabase) {
+    const shortURLObj = urlDatabase[shortURL]
+    if (shortURLObj.userID === ID) {
+      Obj[shortURL] = shortURLObj
+    }
+  } 
+  return Obj
+} 
+
 app.get("/urls", (req, res) => {
 
-  const user = users[req.cookies["user_id"]]
-
-  const templateVars = { urls: urlDatabase, user};
-  console.log("user",templateVars.user);
-  console.log(users);
-  console.log(req.cookies)
-
+  const ID = req.cookies["user_id"]
+  const user = users[ID]
+  const templateVars = { urls: UrlofUser(ID), user};
+  // console.log("user",templateVars.user);
+  // console.log(users);
+  // console.log(req.cookies)
+  console.log('these are my urls', UrlofUser((user)))
+  console.log('this is the database', urlDatabase)
   res.render("urls_index", templateVars);
   // console.log(req.cookies("user_id"));
 });
@@ -103,27 +113,41 @@ app.get("/urls/new", (req, res) => {
 // In our request object, we need req.params and req.body
 // this route is going to lead us to our short URL page, aka urls_show
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]]};
-  console.log(req.params)
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longUrl, user: users[req.cookies["user_id"]]};
+  // console.log(req.params)
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  // console.log(req.body);  // Log the POST request body to the console
   // res.send("Ok");  
+  const userID = req.cookies["user_id"]
   const randomString = generateRandomString()  
-  urlDatabase[randomString] = req.body.longURL 
+  urlDatabase[randomString] = {longUrl: req.body.longURL, userID} 
+  console.log('this is my userID', userID);
   res.redirect('/urls')
   // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  console.log('url',urlDatabase);
-  const longURL = urlDatabase[req.params.shortURL]
+  // console.log('url',urlDatabase);
+  const longURL = urlDatabase[req.params.shortURL].longUrl
   res.redirect(longURL);
 });
 
+// const UrlofUser = function(ID) {
+//   const Obj = {}
+//   for (const shortURL in urlDatabase) {
+//     const shortURLObj = urlDatabase[shortURL]
+//     if (shortURLObj.userID === ID) {
+//       Obj[shortURL] = shortURLObj
+//     }
+//   } 
+//   return Obj
+// } 
+
 app.post("/urls/:shortURL/delete", (req,res) => {
+  
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
 })
@@ -139,7 +163,7 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req,res) => {
-  console.log(req.body, "req");
+  // console.log(req.body, "req");
   const newuserId = generateRandomString()
 // 1. Extract the user info from the form.
   const email = req.body.email
